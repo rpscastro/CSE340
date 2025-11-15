d04d58a9c3f9b93a3d436f12e07d5b30d959884f
Rafael Pereira de Souza Castro <rcastro2@byupathway.edu>
2025-11-15 20:11:23 -0300

    Assignment 3 - Vehicle Detail View - fix build

--- utilities/index.js
+++ utilities/index.js
@@ -6,7 +6,7 @@ const Util = {}
  ************************** */
 Util.getNav = async function (req, res, next) {
   let data = await invModel.getClassifications()
-  let list = '<ul id="nav">'
+  let list = "<ul>"
   list += '<li><a href="/" title="Home page">Home</a></li>'
   data.rows.forEach((row) => {
     list += "<li>"
