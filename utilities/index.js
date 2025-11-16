1fd1d4120592986d2ca35d37e336227024b8d1e2
Rafael Pereira de Souza Castro <rcastro2@byupathway.edu>
2025-11-15 21:04:34 -0300

    Wave validation fix

--- utilities/index.js
+++ utilities/index.js
@@ -36,7 +36,7 @@ Util.buildClassificationGrid = async function(data){
       grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
       + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
       + 'details"><img src="' + vehicle.inv_thumbnail 
-      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+      +'" alt="'+ vehicle.inv_make + ' ' + vehicle.inv_model 
       +' on CSE Motors" /></a>'
       grid += '<div class="namePrice">'
       grid += '<hr />'
@@ -66,7 +66,7 @@ Util.buildDetailsGrid = async function (data) {
       grid = '<div id="vehicle-details">'
       grid += '<div class="vehicle-image">'
       grid +=  '<picture><source media="(min-width: 700px)" srcset="'+ vehicle.inv_image +'">'  
-      + '<img src="'+ vehicle.inv_image +'" loading="lazy" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+      + '<img src="'+ vehicle.inv_image +'" loading="lazy" alt="'+ vehicle.inv_make + ' ' + vehicle.inv_model 
       +' on CSE Motors"> </picture>  </div>'
       grid += '<div class="vehicle-info">'
       grid += '<h2>'+ vehicle.inv_make + ' ' + vehicle.inv_model + ' Details </h2>'
